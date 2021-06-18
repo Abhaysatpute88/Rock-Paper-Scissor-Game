dfe21245a50c0e414dce92587578d8912e224f6c
ABHAY <85785781+Abhaysatpute88@users.noreply.github.com>
2021-06-18 16:03:04 +0530

    break

--- prob8.py
+++ prob8.py
@@ -12,18 +12,21 @@ for i in range(1,7):
         computer=random.choice(list1)
         list2=["rock","paper","scissor"]
         a=int(input("enter the number:"))
+        if a>2:
+            print("invalid input")
+            break
         b=list2[a]
         print(f"because of opponent character is {computer} and your character {b}")
-        if computer==b:
-              print("both the characters are same play again")
-        elif (b=="paper" and computer=="rock") or (b=="scissor" and computer=="paper") or (b=="rock" and computer=="scissor"):
-             print("you win the game")
-             k=(k+1)
-             print(f"you got {k} points")
+        if computer == b:
+            print("both the characters are same play again")
+        elif (b == "paper" and computer == "rock") or (b == "scissor" and computer == "paper") or (b == "rock" and computer == "scissor"):
+            print("you win the game")
+            k = (k + 1)
+            print(f"you got {k} points")
         else:
-             print("opponent win the game")
-             p=(p+1)
-             print(f"opponent got {p} points")
+            print("opponent win the game")
+            p = (p + 1)
+            print(f"opponent got {p} points")
 
 if k>p:
     print(f"your points are {k} you won the game......opponent points are {p}")
@@ -31,6 +34,19 @@ elif p>k:
     print(f"opponent points are {p} opponent won the game......your points are {k}")
 else:
     print(f"your points {k} and opponent points {p} game is draw")
+print("want to play again press yes/no")
+user=str(input())
+if user==yes or user==YES or user==Yes:
+
+
+
+
+
+
+
+
+
+
 
 
 
